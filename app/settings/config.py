5135814ed16f04494056bdc3a3d719bea3373864
guilhermebranddao <guilhermebranddao@gmail.com>
2025-01-06 15:27:37 -0300

    fix(config.py): env to none

--- app/settings/config.py
+++ app/settings/config.py
@@ -6,7 +6,8 @@ from pydantic_settings import BaseSettings, SettingsConfigDict
 
 class Settings(BaseSettings):
     model_config = SettingsConfigDict(
-        env_file='.env', env_file_encoding='utf-8'
+        env_file=str(Path(".env")) if Path(".env").exists() else None,
+        env_file_encoding='utf-8'
     )
 
     DATABASE_URL: str
